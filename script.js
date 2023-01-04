ca79338c1c438ff085a58735902ef95ce9969deb
JMess87 <116509586+JMess87@users.noreply.github.com>
2023-01-04 16:54:47 -0500

    updated js file, readme, script and style.css. Completed functionality as desired. Will be doing some styling later on.

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-// Variables
-
-// Save Button
-var saveBtn = document.getElementsByClassName("saveBtn");
-// For todays date function
-var today = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(today);
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+//  Set date into "todayDate by using dayjs "
+var todaysDate = dayjs().format("dddd, MMMM D, YYYY h:mm A");
+console.log(todaysDate)
+$("#todaysDate").append(todaysDate);
 
 
+// function to set colors of hour sections based on the current hour of the day
+function colors() {
+    // get current time
+    var hourNow = dayjs().hour();
+    // loop to run through each time block and determine which condition is met
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        if (blockHour < hourNow) {
+            $(this).addClass("past");
+        }
+        else if (blockHour === hourNow) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+// Calling the colors function
+colors();
+
+// Jquery for saveBtn event listener
+$(".saveBtn").on("click", function () {
+    var event = $(this).siblings(".description").val();
+    var eventTime = $(this).parent().attr("id");
+
+    // save in localStorage upon click
+    localStorage.setItem(eventTime, event);
+})
+
+
+
+
+// Repop previously saved information
+$("#hour-6 .description").val(localStorage.getItem("hour-6"));
+$("#hour-7 .description").val(localStorage.getItem("hour-7"));
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
